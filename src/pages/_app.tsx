5527f6efc4baa50e24c250cf04af72fec36cab92
Mintemir777 <mintemir2004@mail.ru>
2023-10-30 21:24:22 +0600

    Improved the code as stated in the review

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,19 @@ import "@/styles/globals.css";
 import { themeObject } from "@/util";
 import { ThemeProvider, createTheme } from "@mui/material";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import Navbar from "@/components/NavBar/index";
 
 export default function App({ Component, pageProps }: AppProps) {
   const theme = createTheme(themeObject());
+
+  const router = useRouter();
+  const isLoginOrSignUpPage =
+    router.pathname === "/auth/login" || router.pathname === "/auth/sign-up";
+
   return (
     <ThemeProvider theme={theme}>
+      {!isLoginOrSignUpPage && <Navbar />}
       <Component {...pageProps} />
     </ThemeProvider>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import Navbar from "@/components/NavBar";
 export default function MainPage() {
   return (
     <div>
-      <Navbar/>
       <header className="bg-blue-500 text-white">
         <div className="container mx-auto p-4 text-center">
           <h1 className="text-4xl font-bold">Поиск авиабилетов</h1>
