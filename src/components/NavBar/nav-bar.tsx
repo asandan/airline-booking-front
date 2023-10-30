7d7e26c86246faf5b9e615c21cbffb8ca8f392a0
Mintemir777 <mintemir2004@mail.ru>
2023-10-30 20:55:54 +0600

    Сhanged the code as it says in the review

--- src/components/NavBar/nav-bar.tsx
+++ src/components/NavBar/nav-bar.tsx
@@ -1,12 +1,7 @@
-import React from 'react';
 import Link from 'next/link';
 
-interface NavbarProps {
-  user: string;
-  onSignOut: () => void;
-}
 
-const Navbar: React.FC<NavbarProps> = ({ user, onSignOut }) => {
+const Navbar = () => {
   return (
     <nav className="bg-white shadow-md">
       <div className="container mx-auto flex items-center justify-between p-4">
@@ -16,8 +11,7 @@ const Navbar: React.FC<NavbarProps> = ({ user, onSignOut }) => {
           </h3>
         </Link>
         <div className="flex items-center">
-          <span className="mr-4">{user}</span>
-          <button onClick={onSignOut} className="bg-blue-500 text-white px-3 py-1 rounded-lg">
+          <button className="bg-blue-500 text-white px-3 py-1 rounded-lg">
             Sign Out
           </button>
         </div>
