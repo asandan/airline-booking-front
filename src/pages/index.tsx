b1ff039845c12feb260dc5ca15969fded0d61b6e
asandan <assanali.danybayev@gmail.com>
2023-10-30 21:30:52 +0600

    Removed extra div

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,14 @@
 export default function MainPage() {
   return (
-    <div>
-      <header className="bg-blue-500 text-white">
-        <div className="container mx-auto p-4 text-center">
-          <h1 className="text-4xl font-bold">Airfare search</h1>
-          <div className="my-8 flex justify-center space-x-4">
-            <button className="py-2 px-4 bg-blue-400 text-white rounded-lg">
-              Airline tickets
-            </button>
-          </div>
+    <header className="bg-blue-500 text-white">
+      <div className="container mx-auto p-4 text-center">
+        <h1 className="text-4xl font-bold">Airfare search</h1>
+        <div className="my-8 flex justify-center space-x-4">
+          <button className="py-2 px-4 bg-blue-400 text-white rounded-lg">
+            Airline tickets
+          </button>
         </div>
-      </header>
-    </div>
+      </div>
+    </header>
   );
 }
